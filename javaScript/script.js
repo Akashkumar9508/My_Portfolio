77e2a37054306b44d4b3128d5c1559394d85d81c
Akash <akashbca9508@gmail.com>
2023-12-29 14:22:13 +0530

    bgc add

---
+++ javaScript/script.js
@@ -0,0 +1,18 @@
+var setVanta = ()=>{
+    if (window.VANTA) window.VANTA.NET({
+      el: ".s-page-1 .s-section-1 .s-section",
+      mouseControls: true,
+      touchControls: true,
+      gyroControls: false,
+      minHeight: 200.00,
+      minWidth: 200.00,
+      scale: 1.00,
+      scaleMobile: 1.00,
+      backgroundColor:080e1e,
+    })
+    }
+    _strk.push(function() {
+      setVanta()
+      window.edit_page.Event.subscribe( "Page.beforeNewOneFadeIn", setVanta )
+    })
+    </script>
