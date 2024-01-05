09dab4f0a9de635d23844bb95f5f3ac9c5ba7620
Akash <akashbca9508@gmail.com>
2024-01-05 23:07:45 +0530

    add eduaction page

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,8 +1,13 @@
 const header = document.getElementById("header");
 window.addEventListener("scroll", function () {
-    if (window.scrollY > 200) {
+    if (window.scrollY > 1300) {
+        header.classList.add("sticky2");
+        header.classList.remove("sticky");
+    } else if (window.scrollY > 650 && 1300) {
         header.classList.add("sticky");
+        header.classList.remove("sticky2");
     } else {
         header.classList.remove("sticky");
+        header.classList.remove("sticky2");
     }
 });
